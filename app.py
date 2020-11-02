4920b7d5bc4a075cab9d7799ffffb43321f4d361
xukkz <60608121+xukkz@users.noreply.github.com>
2020-11-02 10:58:18 +0100

    first commit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+
 
 class Lager:
     def __init__(self, namn, pris, antal):
@@ -10,6 +12,13 @@ lagerList = []
 lagerList.append(Lager("Playstation 5 (Physical Edition)", "5990 SEK", "5"))
 lagerList.append(Lager("Playstation 5 (Digital Edition)", "4990 SEK", "3"))
 lagerList.append(Lager("BloodBorne Kart", "420 SEK", "Slutsåld"))
-for lager in lagerList:
-    print('{}\n{}\nLagerstatus: {}\n'.format(
-        lager.namn, lager.pris, lager.antal))
+
+del lagerList[0]
+print("Visa lager?")
+answer = input("Y/y för 'JA' och N/n för 'NEJ\n'")
+if answer == "Y" or answer == "y":
+    for lager in lagerList:
+        print('{}\n{}\nLagerstatus: {}\n'.format(
+            lager.namn, lager.pris, lager.antal))
+elif answer == "N" or answer == "n":
+    sys.exit()
