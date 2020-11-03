cf4920170d25a886e82784a42defcbf5c3817996
xukkz <wiggo.bergman@gmail.com>
2020-11-03 22:27:14 +0100

    fixed basic read and write from file. items are now saved between sessions

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import sys
+from sys import path
+from time import time
 import keyboard
 import os
+import json
+import time
+import os.path
+from keyboard import wait
 
 lagerList = []
+test = {}
+dictb = {}
+
+
+def clear():
+    os.system('cls')
 
 
 class Lager:
@@ -12,31 +24,17 @@ class Lager:
         self.antal = antal
 
 
-try:
-    with open('test.txt', 'r') as myFile:
-        if os.stat('test.txt').st_size == 0:
-            print("Cannot read contents of file. Maybe the file is empty?")
-except FileNotFoundError:
-    print("No file to open. Is there a 'test.txt' in the working directory?")
-
-
-count = 0
-
-#lagerList.append(Lager("Playstation 5 (Physical Edition)", "5990", "5"))
-#lagerList.append(Lager("Playstation 5 (Digital Edition)", "4990", "3"))
-#lagerList.append(Lager("BloodBorne Kart", "420", "Slutsåld"))
-
-
-dictb = {}
-
-for lager in lagerList:
-    dictb[lager] = "{}".format(lager.namn)
-
-
 def addEntry():
-    entry = Lager(input("Namn på produkten: "), input(
-        "Pris på produkten: "), input("Lagerstatus: "))
-    lagerList.append(entry)
+    lagerList.append(Lager(str(input("Namn på produkten: ")), str(input(
+        "Pris på produkten: ")), str(input("Lagerstatus: "))))
+
+    for lager in lagerList:
+        dictb[lager] = "{}#{}#{}".format(
+            lager.namn, lager.pris, lager.antal)
+
+    with open('test.txt', 'w') as temp:
+        for i in dictb:
+            temp.write(str(dictb[i]))
 
 
 def showLogic():
@@ -47,32 +45,56 @@ def showLogic():
             lager.namn, lager.pris, lager.antal))
 
 
+clear()
+if os.path.isfile('test.txt'):
+    print("'test.txt' found!")
+else:
+    print("'test.txt' not found, creating file now...")
+    with open('test.txt', 'w') as create:
+        pass
+
+try:
+    with open('test.txt', 'r') as myFile:
+        if os.stat('test.txt').st_size == 0:
+            print("Cannot read contents of file. Maybe the file is empty?")
+        elif not os.stat('test.txt').st_size == 0:
+            with open('test.txt', 'r') as tempFile:
+
+                for line in tempFile:
+                    (key, val1, val2) = line.split("#")
+                    test[key] = val1, val2
+                    lagerList.append((Lager(key, val1, val2)))
+
+except FileNotFoundError:
+    print("File cound not be opened. File is missing or could be corrupted!")
+    print("Press 'ENTER' to exit the program.")
+    keyboard.wait('enter')
+    sys.exit()
+
+
 print("Visa lager?")
-print("Y/y för 'JA' och N/n för 'NEJ\n'")
+print("Y/y för 'JA' och N/n för 'NEJ'\n")
 
 while True:
     try:
         if keyboard.is_pressed("y"):
-            for lager in lagerList:
+            if os.stat('test.txt').st_size == 0:
+                print("No items to show!")
+            else:
+                for lager in lagerList:
 
-                print('{}\n{} SEK\nLagerstatus: {}\n'.format(
-                    lager.namn, lager.pris, lager.antal))
+                    print('{}\n{} SEK\nLagerstatus: {}st\n'.format(
+                        lager.namn, lager.pris, lager.antal))
             break
         elif keyboard.is_pressed("n"):
-            break
+            sys.exit()
     except:
-        break
+        sys.exit()
 
 print("Vill du lägga till eller ta bort en artikel?")
-for i in dictb:
-    count = int(count + 1)
-    print(str(count) + ": " + dictb[i])
 
-with open('test.txt', 'w') as myFile:
-    count = 0
-    for i in dictb:
-        count = int(count + 1)
-        myFile.write(str(count) + ": " + dictb[i]+"\n")
+addEntry()
+
 
 # while True:
 #   try:
